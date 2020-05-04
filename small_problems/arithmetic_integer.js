514a673a7cd15021c7854ea2af860dd29e6bae34
Daniel Cosme <cosmedaniel8@gmail.com>
2020-05-04 11:19:53 -0400

    Add arithmetic integer

---
+++ small_problems/arithmetic_integer.js
@@ -0,0 +1,59 @@
+/* Write a program that prompts the user for two positive integers, and then prints the results of the following operations on those two numbers: addition, subtraction, product, quotient, remainder, and power. Do not worry about validating the input.
+
+Example
+
+==> Enter the first number:
+23
+==> Enter the second number:
+17
+==> 23 + 17 = 40
+==> 23 - 17 = 6
+==> 23 * 17 = 391
+==> 23 / 17 = 1
+==> 23 % 17 = 6
+==> 23 ** 17 = 141050039560662968926103
+
+*/
+
+const readline = require("readline-sync");
+
+let firstNumber = Number(
+	readline.question(prompt("Enter the first number:\n"))
+);
+let secondNumber = Number(
+	readline.question(prompt("Enter the second number:\n"))
+);
+
+console.log(
+	getOperationString(firstNumber, secondNumber, "+", firstNumber + secondNumber)
+);
+console.log(
+	getOperationString(firstNumber, secondNumber, "-", firstNumber - secondNumber)
+);
+console.log(
+	getOperationString(firstNumber, secondNumber, "/", firstNumber / secondNumber)
+);
+console.log(
+	getOperationString(firstNumber, secondNumber, "*", firstNumber * secondNumber)
+);
+console.log(
+	getOperationString(firstNumber, secondNumber, "%", firstNumber % secondNumber)
+);
+console.log(
+	getOperationString(
+		firstNumber,
+		secondNumber,
+		"**",
+		Math.pow(firstNumber, secondNumber)
+	)
+);
+
+// Functions
+
+function getOperationString(num1, num2, op, result) {
+	return prompt(`${num1} ${op} ${num2} = ${result}`);
+}
+
+function prompt(x) {
+	return `==> ${x}`;
+}
