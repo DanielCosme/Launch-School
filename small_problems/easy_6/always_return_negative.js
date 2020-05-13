a2c50528b31a96e4080a68281fb367eacba3dd08
Daniel Cosme <cosmedaniel8@gmail.com>
2020-05-13 10:30:41 -0400

    Add return negative and counting up

---
+++ small_problems/easy_6/always_return_negative.js
@@ -0,0 +1,13 @@
+/* Write a function that takes a number as an argument. If the argument is a positive number, return the negative of that number. If the argument is a negative number, return it as-is.
+
+Examples: */
+
+negative(5);     // -5
+negative(-3);    // -3
+negative(0);     // -0
+
+function negative(num) {
+  let result = Math.abs(num) * -1;
+  console.log(result);
+  return result;
+}
