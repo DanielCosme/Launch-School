2abf45bae2bfd4162cfdf509201ba444420a9b15
Daniel Cosme <cosmedaniel8@gmail.com>
2020-05-13 10:55:22 -0400

    Add name swapping

---
+++ small_problems/easy_6/name_swapping.js
@@ -0,0 +1,22 @@
+/* Write a function that takes a string argument consisting of a first name, a space, and a last name, and returns a new string consisting of the last name, a comma, a space, and the first name.
+
+Examples: */
+
+swapName('Joe Roberts');    // "Roberts, Joe"
+
+
+function swapName(str) {
+  let swap = str.split(" ").reverse();
+  let first = swap[0];
+  let second = swap.slice(1).join(" ");
+  let result = `${first}, ${second}`;
+
+  // console.log(result);
+  return result;
+}
+
+/* Further Exploration
+
+What if the person has one or more middle names? Refactor the current solution so that it can accommodate this; the middle names should be listed after the first name: */
+
+swapName('Karl Oskar Henriksson Ragvals');    // "Ragvals, Karl Oskar Henriksson"
