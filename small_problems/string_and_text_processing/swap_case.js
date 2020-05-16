300d5fca74a2c5144096fe2168e84aa11d586e49
Daniel Cosme <cosmedaniel8@gmail.com>
2020-05-15 21:33:53 -0400

    Add swap case

---
+++ small_problems/string_and_text_processing/swap_case.js
@@ -0,0 +1,22 @@
+/* Write a function that takes a string as an argument, and returns that string with every lowercase letter changed to uppercase and every uppercase letter changed to lowercase. Leave all other characters unchanged.
+
+Examples:
+*/
+
+swapCase('CamelCase');              // "cAMELcASE"
+swapCase('Tonight on XYZ-TV');      // "tONIGHT ON xyz-tv"
+
+function swapCase(str) {
+  let ret = '';
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (char.toUpperCase() === char) ret += char.toLowerCase();
+    else ret += char.toUpperCase();
+  }
+
+  console.log(ret);
+  return ret;
+}
+
+
