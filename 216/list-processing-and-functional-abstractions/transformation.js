d8531ed266f5cf84a99fb66738b8e595acaa2988
Daniel Cosme <cosmedaniel8@gmail.com>
2023-10-26 19:22:41 -0400

    Add html-css module

--- 216/list-processing-and-functional-abstractions/transformation.js
+++ 216/list-processing-and-functional-abstractions/transformation.js
@@ -1,5 +1,5 @@
 let count = [1, 2, 3, 4, 5];
-let doubled = count.map((number, index, array) => number * 2); // double each number
+let doubled = count.map((number) => number * 2); // double each number
 console.log(doubled);                                          // logs [ 2, 4, 6, 8, 10 ]
 console.log("")
 
---
+++ html-css/notes.md
@@ -0,0 +1,5 @@
+(W3C html validator)[https://validator.w3.org/#validate_by_input]
+(W3C css validator)[https://jigsaw.w3.org/css-validator/#validate_by_input]
+
+https://html5doctor.com/
+
